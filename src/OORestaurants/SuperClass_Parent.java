fb9148e61f279a01969999b67f38f2e17fe2a8bb
Leonard Sangoroh <leonardsangoroh@gmail.com>
2021-04-11 10:43:48 +0300

    initial

---
+++ src/OORestaurants/SuperClass_Parent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OORestaurants;
+
+/**
+ *
+ * @author leesangoroh
+ */
+public class SuperClass_Parent {
+    
+    public double ChickenBurger;
+    public double FiletOFish;
+    public double ChickenLegend;
+    public double ChickenBurgerMeal;
+    public double BaconCheeseBurger;
+    
+    
+    
+    
+}
